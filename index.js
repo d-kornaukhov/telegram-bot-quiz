76ec47a5f97dc72e86fe5a31cc65e795deee582d
Cloackz <Cloackz@yandex.ru>
2023-06-22 13:24:22 +0400

    Reply buttons in question and some fix

--- index.js
+++ index.js
@@ -1,134 +1,70 @@
-const TelegramBot = require('node-telegram-bot-api')
-const TOKEN = '5600917968:AAHT6bsKHrqweVU_Ufqq4z0HgaJ5i26K2XQ'
+const TelegramBot = require("node-telegram-bot-api")
+const TOKEN = "5600917968:AAHT6bsKHrqweVU_Ufqq4z0HgaJ5i26K2XQ"
+// import { config } from 'dotenv'
+// import express from 'express'
 
 const bot = new TelegramBot(TOKEN, { polling: true })
 
-var questions = [
-	{
-		text: `
-		<b>Вопрос 1</b>\n
-	  <b>2 + 2</b>\n
-		<b>Варианты ответов</b>\n
-	  1️⃣  2\n
-	  2️⃣  22\n
-	  3️⃣  4
-		`,
-		buttons: [
-			[
-				{
-					text: 'Ответ 1',
-					callback_data: '0_1',
-				},
-			],
-			[
-				{
-					text: 'Ответ 2',
-					callback_data: '0_2',
-				},
-			],
-			[
-				{
-					text: 'Ответ 3',
-					callback_data: '0_3',
-				},
-			],
-		],
-		right_answer: 3,
-		id: 1,
-	},
-	{
-		text: `
-		<b>Вопрос 2</b>\n
-	  <b>Название нашей планеты</b>\n
-		<b>Варианты ответов</b>\n
-			1️⃣  Юпитер\n
-			2️⃣  Земля\n
-			3️⃣  Меркурий\n
-			4️⃣  Меркурий\n
-		`,
-		buttons: [
-			[{ text: 'Ответ 1', callback_data: '1_1' }],
-			[{ text: 'Ответ 2', callback_data: '1_2' }],
-			[{ text: 'Ответ 3', callback_data: '1_3' }],
-			[{ text: 'Ответ 4', callback_data: '1_4' }],
-		],
-		right_answer: 2,
-		id: 2,
-	},
-	{
-		text: `
-		<b>Вопрос 3</b>\n
-	  <b>Столица Японии</b>\n
-		<b>Варианты ответов</b>\n
-			1️⃣  Ереван\n
-			2️⃣  Тайбэй\n
-			3️⃣  Токио\n
-			4️⃣  Рига\n
-		`,
-		buttons: [
-			[{ text: 'Ответ 1', callback_data: '2_1' }],
-			[{ text: 'Ответ 2', callback_data: '2_2' }],
-			[{ text: 'ответ 3', callback_data: '2_3' }],
-			[{ text: 'Ответ 4', callback_data: '2_4' }],
-		],
-		right_answer: 3,
-		id: 3,
-	},
-]
-
 let currentIndex = 0
 let correctAnswers = 0
+let testInProgress = false
+let scores = 0
 
-const initialeStateTest = () => {
+const initializeTest = () => {
 	currentIndex = 0
 	correctAnswers = 0
+	scores = 0
+	testInProgress = true
 }
 
 const getNextQuestion = () => {
 	const nextQuestion = questions[currentIndex]
-	currentIndex = currentIndex % questions.length
+	currentIndex++
 	return nextQuestion
 }
 
 const newQuestion = async msg => {
+	if (!testInProgress) return
+
 	const question = getNextQuestion()
 	if (!question) {
-		const percentage = (correctAnswers / questions.length) * 100
-		const resultMessage = `Тест завершен! Вы набрали ${percentage}% правильных ответов. Введите команду /test или нажмите кнопку ниже, чтобы перепройти тест`
+		testInProgress = false
+		let resultMessage
+		resultMessage = `Тест завершен! вы набрали ${scores} баллов. Введите команду /test или нажмите кнопку ниже, чтобы перепройти тест`
 
-		await bot.sendMessage(msg.chat.id, resultMessage)
-		initialeStateTest()
+		const startButton = [{ text: "/test" }]
+		const shareContactButton = [
+			{ text: "Поделится контактом", request_contact: true },
+		]
+		const options = {
+			reply_markup: {
+				keyboard: [startButton, shareContactButton],
+				resize_keyboard: true,
+				one_time_keyboard: true,
+			},
+		}
+		await bot.sendMessage(msg.chat.id, resultMessage, options)
+
 		return
 	}
 
 	const text = question.text
 	const options = {
 		reply_markup: {
-			inline_keyboard: question.buttons,
+			keyboard: question.buttons,
+			resize_keyboard: true,
 		},
-		parse_mode: 'HTML',
+		parse_mode: "HTML",
 	}
-	const chat = msg.hasOwnProperty('chat') ? msg.chat.id : msg.from.id
+	const chat = msg.hasOwnProperty("chat") ? msg.chat.id : msg.from.id
 
 	await bot.sendMessage(chat, text, options)
 }
 
-bot.on('callback_query', async query => {
-	const selectedOption = query.data.split('_')[1]
-	const currentQuestion = questions[currentIndex]
-
-	if (parseInt(selectedOption) === currentQuestion.right_answer) {
-		correctAnswers++
-	}
-	currentIndex++
-
-	await newQuestion(query.message)
-})
-
 bot.onText(/^$/, async msg => {
 	const infoMessage =
 		'Добро пожаловать в наш бот! Нажмите кнопку "Начать тест", чтобы начать тестирование.'
-	const startButton = [{ text: '/test' }]
+	const startButton = [{ text: "/test" }]
 	const options = {
 		reply_markup: {
 			keyboard: [startButton],
@@ -140,7 +76,7 @@ bot.onText(/^$/, async msg => {
 })
 
 bot.onText(/\/start/, async msg => {
-	const startButton = [{ text: '/test' }]
+	const startButton = [{ text: "/test" }]
 	const options = {
 		reply_markup: {
 			keyboard: [startButton],
@@ -153,11 +89,84 @@ bot.onText(/\/start/, async msg => {
 		'Нажмите кнопку "Начать тест", чтобы начать тест.',
 		options
 	)
-
-	initialeStateTest()
 })
 
 bot.onText(/\/test/, async msg => {
-	initialeStateTest()
+	initializeTest()
 	await newQuestion(msg)
 })
+
+bot.on("message", async msg => {
+	if (!testInProgress) return
+
+	const selectedOption = msg.text
+	const currentQuestion = questions[currentIndex - 1]
+
+	if (currentQuestion && selectedOption === currentQuestion.right_answer) {
+		correctAnswers++
+		scores += 5
+	} else {
+		scores -= 3
+	}
+	if (scores < 0) scores = 0
+
+	await newQuestion(msg)
+})
+
+const questions = [
+	{
+		text: `
+<b>Вопрос 1</b>\n
+<b>2 + 2</b>\n
+<b>Варианты ответов</b>\n
+1️⃣  2\n
+2️⃣  22\n
+3️⃣  4
+        `,
+		buttons: [
+			[{ text: "Ответ 1" }],
+			[{ text: "Ответ 2" }],
+			[{ text: "Ответ 3" }],
+		],
+		right_answer: "Ответ 3",
+		id: 1,
+	},
+	{
+		text: `
+<b>Вопрос 2</b>\n
+<b>Название нашей планеты</b>\n
+<b>Варианты ответов</b>\n
+1️⃣  Юпитер\n
+2️⃣  Земля\n
+3️⃣  Меркурий\n
+4️⃣  Венера
+        `,
+		buttons: [
+			[{ text: "Ответ 1" }],
+			[{ text: "Ответ 2" }],
+			[{ text: "Ответ 3" }],
+			[{ text: "Ответ 4" }],
+		],
+		right_answer: "Ответ 2",
+		id: 2,
+	},
+	{
+		text: `
+<b>Вопрос 3</b>\n
+<b>Столица Японии</b>\n
+<b>Варианты ответов</b>\n
+1️⃣  Ереван\n
+2️⃣  Тайбэй\n
+3️⃣  Токио\n
+4️⃣  Рига
+        `,
+		buttons: [
+			[{ text: "Ответ 1" }],
+			[{ text: "Ответ 2" }],
+			[{ text: "Ответ 3" }],
+			[{ text: "Ответ 4" }],
+		],
+		right_answer: "Ответ 3",
+		id: 3,
+	},
+]
